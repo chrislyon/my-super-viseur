7c346dd2b4167df921ec4145d4b280fa5e59c43d
chris <chris@localhost>
2013-02-27 17:42:04 +0100

    Test de commit

--- t/t2.py
+++ t/t2.py
@@ -9,10 +9,15 @@ scanner = re.Scanner([
     (r"[a-zA-Z_]\w*", identifier),
     (r"\+|\-|\\|\*|\=", operator),
     (r"[0-9]+(\.[0-9]+)?", digit),
-    (r";", end_stmnt),
+    (r"\n", end_stmnt),
     (r"\s+", None),
     ])
+
+to_scan = """
+foo = 5 * 30
+bar = bar - 60
+"""
  
-tokens, remainder = scanner.scan("foo = 5 * 30; bar = bar - 60;")
+tokens, remainder = scanner.scan(to_scan)
 for token in tokens:
     print token
